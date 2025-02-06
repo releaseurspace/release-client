e6a1524ac03a9a38a713d8a9adaa164c3466bb20
chelsh <sinye2002@korea.ac.kr>
2025-02-06 16:55:03 +0900

    ✨ Readablestream tester

--- app/(main)/report/page.tsx
+++ app/(main)/report/page.tsx
@@ -5,13 +5,28 @@ import { useState } from "react";
 
 export default function Home() {
   const [tokens, setTokens] = useState<string[]>([]);
+  const [question, setQuestion] = useState<string>("");
+  const [userId, setUserId] = useState<string>("");
 
   return (
     <div className="h-[100vh]">
       <NavBar />
 
+      <input
+        value={userId}
+        placeholder="userID"
+        className="border border-1"
+        onChange={(e) => setUserId(e.target.value)}
+      ></input>
+      <input
+        value={question}
+        placeholder="질문하기"
+        className="border border-1"
+        onChange={(e) => setQuestion(e.target.value)}
+      ></input>
+
       <button
-        className="bg-yellow-500 mt-20 ml-20 w-24 h-24"
+        className="bg-yellow-500 mt-20 ml-20 w-12 h-12"
         onClick={async () => {
           const res = await fetch(
             "https://3.38.128.30.nip.io/langchain/stream",
@@ -21,11 +36,12 @@ export default function Home() {
                 "Content-Type": "application/json",
               },
               body: JSON.stringify({
-                userId: "1",
-                content: "안녕! 성수쪽에서 매물 찾아줘",
+                userId: "99",
+                content: question,
               }),
             }
           );
+          setQuestion("");
 
           console.log("res", res);
 
@@ -46,7 +62,7 @@ export default function Home() {
               try {
                 const parsed = JSON.parse(trimmed);
                 console.log("parsed", parsed);
-                setTokens((prev) => [...prev, parsed.token])
+                setTokens((prev) => [...prev, parsed.token]);
               } catch (e) {
                 console.error("JSON 파싱 에러:", e, trimmed);
               }
@@ -57,6 +73,7 @@ export default function Home() {
         call api
       </button>
 
+      <div className="mt-10">답변:</div>
       <div>
         {tokens.map((token, idx) => (
           <span key={idx}>{token}</span>
